268990c462950e68af6171e8d21ee2912ee593b1
zhangxingxing189 <2200955712@qq.com>
2025-01-23 18:00:56 +0800

    把hometest放page里面

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import BasicLayout from "./layouts/BasicLayout.vue";
 import loginPage from "@/pages/userPages/loginPage.vue";
-import GamePage from "@/HomeTest/gamePage.vue";
-import BackgroundTest from "@/HomeTest/backgroundTest.vue";
+import GamePage from "@/pages/HomeTest/gamePage.vue";
+import BackgroundTest from "@/pages/HomeTest/backgroundTest.vue";
 import loginPage_reset from "./pages/userPages/loginPage_reset.vue";
 </script>
 
